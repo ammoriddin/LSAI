430ae0de2b5f03d32fa4d6b58651ff09e55d4fae
Ammoriddin <hakimjavoh@gmail.com>
2024-02-25 18:04:18 +0500

    Add button

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import logo from '../../assets/logo.jpg'
-import { Label, TextInput } from 'flowbite-react'
+import { Button, Label, TextInput } from 'flowbite-react'
 import { Link } from 'react-router-dom'
 
 export default function Login() {
   return (
     <section className='w-full h-[90vh] flex justify-center items-center'>
-        <div className='w-[800px]'>
+        <div className='w-[600px]'>
             <div className='flex mb-4 items-center gap-5 justify-center'>
                 <div className='w-[300px] h-[100px]'>
                     <img className='w-full h-full object-cover' src={logo} alt="logo" />
@@ -23,7 +23,10 @@ export default function Login() {
                     <Label htmlFor='password' className='mb-2 cursor-pointer text-[18px]'>Password</Label>
                     <TextInput type='password' id='password' name='password' placeholder='parolingizni kiriting' />
                 </div>
-                <Link to={'/forgot-password'} className='text-blue-700 font-[500] underline'>Parolingizni unutdingizmi?</Link>
+                <Button className='px-[10px] mx-auto mb-4'>Kirish</Button>
+                <div className='flex justify-center'>
+                    <Link to={'/forgot-password'} className='text-blue-700 font-[500] text-[15px] underline'>Parolingizni unutdingizmi?</Link>
+                </div>
             </form>
         </div>
     </section>
