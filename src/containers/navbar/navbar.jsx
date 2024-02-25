adabd00132e192d35fa5479ae6addd1f1e76e3f7
Ammoriddin <hakimjavoh@gmail.com>
2024-02-25 13:33:57 +0500

    update navbar

--- src/containers/navbar/navbar.jsx
+++ src/containers/navbar/navbar.jsx
@@ -1,14 +1,13 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.jpg'
 import React from 'react'
-import { Button } from 'flowbite-react'
 
 export default function Navbar() {
   return (
     <nav className='w-full border-b-[1.5px] max-w-[1400px] py-[20px] px-[50px] mx-[auto] flex justify-between items-center'>
-        <div className='w-[150px] h-[50px]'>
+        <Link to={'/'} className='w-[150px] h-[50px]'>
             <img className='w-full h-full object-cover' src={logo} alt="logo" />
-        </div>
+        </Link>
         <ul className='flex gap-8 font-[500] text-[18px]'>
             <li>
                 <Link>Courses</Link>
