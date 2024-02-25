27fec34d78389281c29a4276fe0e14593da26307
Ammoriddin <hakimjavoh@gmail.com>
2024-02-25 18:07:47 +0500

    Add create account button

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import logo from '../../assets/logo.jpg'
-import { Label, TextInput } from 'flowbite-react'
+import { Button, Label, TextInput } from 'flowbite-react'
 import { Link } from 'react-router-dom'
 
 export default function Singup() {
   return (
     <section className='w-full h-[90vh] flex justify-center items-center'>
-        <div className='w-[800px]'>
+        <div className='w-[600px]'>
             <div className='flex mb-4 items-center gap-5 justify-center'>
                 <div className='w-[300px] h-[100px]'>
                     <img className='w-full h-full object-cover' src={logo} alt="logo" />
@@ -29,7 +29,10 @@ export default function Singup() {
                     <Label htmlFor='password' className='mb-2 cursor-pointer text-[18px]'>Password</Label>
                     <TextInput type='password' id='password' name='password' placeholder='parolingizni kiriting' />
                 </div>
-                <Link to={'/login'} className='text-blue-700 font-[500] underline'>accountingiz bormi?</Link>
+                <Button className='px-[10px] mx-auto mb-4'>Ro'yhatdan o'tish</Button>
+                <div className='flex justify-center'>
+                    <Link to={'/login'} className='text-blue-700 font-[500] underline'>accountingiz bormi?</Link>
+                </div>
             </form>
         </div>
     </section>
