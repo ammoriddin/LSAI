aa34b4450c11eb1b3156c2a5c894e44eebebc0c1
Ammoriddin <hakimjavoh@gmail.com>
2024-03-01 11:03:19 +0500

    Create join quiz page

--- src/containers/quiz-page/main.jsx
+++ src/containers/quiz-page/main.jsx
@@ -1,15 +1,19 @@
 import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function QuizPage() {
-  return (
-    <section>
-        <div className='container'>
-            <div className='w-full gap-5 h-[80vh] flex items-center justify-center'>
-                <TextInput className='w-[50%]' placeholder='Quiz Code' />
-                <Button className='px-[15px]'>Kirish</Button>
+    const navigate = useNavigate()
+    const [quizCode, setQuizCode] = useState('')
+
+    return (
+        <section>
+            <div className='container'>
+                <div className='w-full gap-5 h-[80vh] flex items-center justify-center'>
+                    <TextInput value={quizCode} onChange={(e) => setQuizCode(e.target.value)} className='w-[50%]' placeholder='Quiz Code' />
+                    <Button onClick={() => navigate(`/quiz/${quizCode}`)} className='px-[15px]'>Kirish</Button>
+                </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
